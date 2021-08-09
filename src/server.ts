58290315453307f228bc076b1f045b1880e08169
raksit31667 <raksit.m@ku.th>
2021-08-09 21:12:28 +0700

    🔊 add pino logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express"
+import pinoHttp from "pino-http"
+import { logger } from './logger'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
+server.use(pinoHttp({ logger }))
 
 server.get("/", (req: Request, res: Response) => {
     res.send("OK")
